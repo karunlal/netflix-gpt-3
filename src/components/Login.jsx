5fafb9567709e6bb4dad685d65ae4d8c3dfa345f
karunlal <karunlalp@gmail.com>
2024-05-21 21:21:28 +0530

    netflix logo added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
     <div>
-      <img
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-        alt="background image"
-      />
+      <Header />
+      <div>
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="background image"
+        />
+      </div>
     </div>
   )
 }
