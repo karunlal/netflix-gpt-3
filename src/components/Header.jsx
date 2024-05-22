15a4a0f883fc70bc3b1653f86b91b5a5c6eab1a2
karunlal <karunlalp@gmail.com>
2024-05-22 16:53:15 +0530

    sign in and sign up updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import { LOGO } from '../utils/constants'
+import { USER_AVATAR } from '../utils/constants'
 
 const Header = () => {
   return (
-    <div className="absolute w-44">
-      <img
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="Logo"
-      />
+    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from black z-10 flex justify-between">
+      <img className="w-44" src={LOGO} alt="Logo" />
+      <div className="flex p-2">
+        <img className="w-12 h-12" src={USER_AVATAR} alt="usericon" />
+        <button className="font-bold text-white font-bold">Sign Out</button>
+      </div>
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,54 @@
 import Header from './Header'
-
+import { useState } from 'react'
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true)
+  const toggleSignInForm = () => {
+    setIsSignInForm((prevState) => !prevState)
+    console.log(isSignInForm)
+  }
+
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="background image"
         />
       </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80 "
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? 'Sign In' : 'Sign Up'}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="name"
+            placeholder="Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button className="p-4 my-6 bg-red-700 w-full">
+          {isSignInForm ? 'Sign In' : 'Sign Up'}
+        </button>
+        <p className="py-6 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? 'New to Netflix? Sign Up Now'
+            : 'Already registered? Sign In now?'}
+        </p>
+      </form>
     </div>
   )
 }
